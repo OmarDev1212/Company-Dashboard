1c77a70c36ec1883ff4815a0af3edbdb0fabbd5c
3omar63 <om728526@gmail.com>
2025-04-18 07:26:13 +0200

    refactored getall in generic repo to achieve soft delete

--- Demo.BLL/Repositories/repos/GenericRepository.cs
+++ Demo.BLL/Repositories/repos/GenericRepository.cs
@@ -1,6 +1,7 @@
 ﻿using Demo.BLL.Repositories.interfaces;
 using Demo.DAL.Data;
 using Demo.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,12 +26,15 @@ namespace Demo.BLL.Repositories.repos
 
         public IEnumerable<T> GetAll(bool withTracking = false)
         {
-            return _dbContext.Set<T>().ToList();
+            if (withTracking)
+                return _dbContext.Set<T>().Where(e => e.IsDeleted != true).ToList();
+            else
+                return _dbContext.Set<T>().AsNoTracking().Where(t => t.IsDeleted != true).ToList();
         }
 
         public T GetEntityById(int id)
         {
-           return  _dbContext.Set<T>().Find(id);
+            return _dbContext.Set<T>().Find(id);
         }
 
         public int UpdateEntity(T entity)
